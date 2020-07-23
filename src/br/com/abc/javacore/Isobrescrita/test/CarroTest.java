d15d5acc758e91575b935861ca161a1cd10de4b7
zeleandroxavier <zeleandroxavier@gmail.com>
2020-07-23 20:01:38 -0300

    aula 56

---
+++ src/br/com/abc/javacore/Isobrescrita/test/CarroTest.java
@@ -0,0 +1,12 @@
+package br.com.abc.javacore.Isobrescrita.test;
+
+public class CarroTest {
+
+    private String nome;
+    private String cpf;
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+
+}
